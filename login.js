37810b4c1ea140d6f35a542fd1e2c992a4b96c1c
TanvirHNur <tanvir.du.ier@gmail.com>
2021-08-16 22:34:45 +0600

    login function added

--- login.js
+++ login.js
@@ -10,3 +10,38 @@ document.getElementById('login-submit').addEventListener('click', function (){
 })
 
 
+
+// duplicate
+document.getElementById('login-submit').addEventListener('click', function (){
+    // get user email
+    const emailFeild = document.getElementById('user-email')
+    const userEmail = emailFeild.value
+    const userPassword = document.getElementById('user-password').value
+// checking
+    if(userEmail == 'child@fathergmail.com' && userPassword == 'secret'){
+        window.location.href = 'banking.html'
+    }
+})
+document.getElementById('login-submit').addEventListener('click', function (){
+    // get user email
+    const emailFeild = document.getElementById('user-email')
+    const userEmail = emailFeild.value
+    const userPassword = document.getElementById('user-password').value
+// checking
+    if(userEmail == 'child@fathergmail.com' && userPassword == 'secret'){
+        window.location.href = 'banking.html'
+    }
+})
+document.getElementById('login-submit').addEventListener('click', function (){
+    // get user email
+    const emailFeild = document.getElementById('user-email')
+    const userEmail = emailFeild.value
+    const userPassword = document.getElementById('user-password').value
+// checking
+    if(userEmail == 'child@fathergmail.com' && userPassword == 'secret'){
+        window.location.href = 'banking.html'
+    }
+})
+
+
+
